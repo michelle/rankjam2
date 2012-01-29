360bd8ee3fb135a66a975059520773a092498ec3
Eric Zhang <really.ez@gmail.com>
2012-01-28 23:56:38 -0800

    lol

---
+++ public/js/index.js
@@ -0,0 +1,70 @@
+var id;
+var token;
+
+
+
+$(document).ready(function(){
+  $('#like').click(function(){
+    now.vote(id, token);
+    $('#like').prop('disabled', true);
+  });
+  
+  now.change = function(title, i) {
+    $('#like').prop('disabled', false);
+    $('#cur').text(title);
+    id = i;
+  }
+
+  now.finish = function(scores){
+    $('#body').empty();
+    console.log(scores);
+    scores.sort(function sortNumber(a,b)  {
+      return a.score - b.score;
+    });
+    for(var i in scores) {
+      $("<h1></h1>").text(scores[i].title + ": " + scores[i].score + " likes").appendTo('#body');
+    }
+  }
+});
+
+window.fbAsyncInit = function() {
+    FB.init({      
+      appId      : '302829396430988', // App ID
+      channelUrl : '//www.petermcottle.com/channel.html', // Channel File
+      status     : true, // check login status
+      cookie     : true, // enable cookies to allow the server to access the session
+      xfbml      : true,  // parse XFBML 
+      oatuh      : true,
+    });            // Additional initialization code here 
+      fbLogin();
+  };
+  // Load the SDK Asynchronously
+  (function(d){
+     var js, id = 'facebook-jssdk'; if (d.getElementById(id)) {return;}
+     js = d.createElement('script'); js.id = id; js.async = true;
+     js.src = "//connect.facebook.net/en_US/all.js";
+     d.getElementsByTagName('head')[0].appendChild(js);
+   }(document));
+
+var fbLoginResponse;
+
+function fbLogin() {
+    FB.login(function(response)
+    {
+        if(response.authResponse) {
+            token = response.authResponse.accessToken;
+            $.getJSON('/token/'+token);
+            fbLoginResponse = response;
+
+            startFacebookStuff();
+
+         } else { 
+             alert('login failed, try again');
+         }
+    },{scope:'user_about_me'});
+}
+
+function startFacebookStuff() {
+    $('#login').fadeOut();
+    $('#body').css({opacity: 1});
+}
--- server.js
+++ server.js
@@ -2,6 +2,9 @@ var express = require('express');
 var fs = require('fs');
 var app =  express.createServer();
 
+var nowjs = require('now');
+var everyone = nowjs.initialize(app);
+
 // Initialize main server
 app.use(express.bodyParser());
 
@@ -15,10 +18,67 @@ app.get('/', function(req, res) {
     res.render('index');
 });
 
-app.post('/submit', function(req, res) {
+app.get('/token/:token', function(req, res){
+  console.log('post', req.params);
+    if(!users[req.params.token]) {
+      users[req.params.token] = {};
+    }
+  res.send('ok');
 });
 
+app.listen(80);
 
-app.listen(8080);
+var users = {};
+
+var current = -1;
+
+var contestants = [
+  'test1', 'test2', 'test3'
+];
+
+everyone.now.vote = function(id, token) {
+  if (users[token]) {
+    users[token][id] = true;
+  }
+}
+
+everyone.now.unvote = function(id, token) {
+  if (users[token]) {
+    delete users[token][id];
+  }
+}
+
+everyone.now.spit = function(){
+  console.log('users', users);
+  console.log('contestants', contestants);
+  console.log('current', current);
+};
+nowjs.on('connect', function(){
+  
+  this.now.change(contestants[current], current);
+
+});
+everyone.now.next = function(pass) {
+  if (pass == '333') {
+    current++;
+    if(current == contestants.length) {
+      var scores = [];
+      for(var token in users) {
+        for(var i in users[token]) {
+          if(users[token][i] === true) {
+            if(scores[i]) {
+              scores[i].score += 1;
+            } else {
+              scores[i] = {title: contestants[i].title, scores: 1};
+            }
+          }
+        }
+      }
+      everyone.now.finish(scores);
+    } else {
+      everyone.now.change(contestants[current], current);
+    }
+  }
+}
 
 
