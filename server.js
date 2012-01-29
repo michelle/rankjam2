2b55fe01bffbb851c574cc2f3bdcb03987cdc565
Michelle Bu <analogmidnight@gmail.com>
2012-01-28 23:02:16 -0800

    small

--- server.js
+++ server.js
@@ -11,8 +11,11 @@ app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 
 
-app.get('/', function(req, res){
+app.get('/', function(req, res) {
     res.render('index');
+});
+
+app.post('/submit', function(req, res) {
 });
 
 
